2de3d1a9f45c6a741b4972b54662220670339a86
Matt Muroya <13994435+mattmuroya@users.noreply.github.com>
2023-03-11 14:47:21 -1000

    logic

---
+++ 4_1_logic.svelte
@@ -0,0 +1,45 @@
+<script>
+  let user = { loggedIn: false };
+
+  function toggle() {
+    user.loggedIn = !user.loggedIn;
+  }
+
+  let x = 7;
+
+  let cats = [
+    { id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+    { id: 'z_AbfPXTKms', name: 'Maru' },
+    { id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' },
+  ];
+</script>
+
+{#if user.loggedIn}
+  <button on:click={toggle}> Log out </button>
+{:else}
+  <button on:click={toggle}> Log in </button>
+{/if}
+
+{#if x > 10}
+  <p>{x} is greater than 10</p>
+{:else if x < 5}
+  <p>{x} is less than 5</p>
+{:else}
+  <p>{x} is between 5 and 10</p>
+{/if}
+
+<h1>The Famous Cats of YouTube</h1>
+
+<ul>
+  {#each cats as cat, i}
+    <li>
+      <a
+        target="_blank"
+        href="https://www.youtube.com/watch?v={cat.id}"
+        rel="noreferrer"
+      >
+        {i + 1}: {cat.name}
+      </a>
+    </li>
+  {/each}
+</ul>
---
+++ 4_2_logic.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Thing from './4_2b_Thing.svelte';
+
+  let things = [
+    { id: 1, name: 'apple' },
+    { id: 2, name: 'banana' },
+    { id: 3, name: 'carrot' },
+    { id: 4, name: 'doughnut' },
+    { id: 5, name: 'egg' },
+  ];
+
+  function handleClick() {
+    things = things.slice(1);
+  }
+</script>
+
+<button on:click={handleClick}> Remove first thing </button>
+
+<!-- By default, when value of `each` block is modified, items are added/removed at the bottom -->
+<!-- Then svelte goes and updates any values that are changed -->
+<!-- Can use unique "Key" identifiers to alter specific DOM nodes -->
+
+{#each things as thing (thing.id)}
+  <Thing name={thing.name} />
+{/each}
